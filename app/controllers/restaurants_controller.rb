cb6617d04097c6ded7dfe99674d6279243524f7e
Nic Rothquel <nrothquel@gmail.com>
2022-08-10 12:28:59 -0400

    index and show created, working on new

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(resto_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Read all
+  get "restaurants", to: "restaurants#index"
+  # Create
+  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "restaurants", to: "restaurants#create"
+  # Read one - The `show` route needs to be *after* `new` route.
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
 end
